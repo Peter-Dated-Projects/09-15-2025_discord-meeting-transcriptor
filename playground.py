9b4a2116c6d230195a521f8061110d02729edf95
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2025-10-29 11:20:07 -0400

    ft. playground

--- playground.py
+++ playground.py
@@ -1,8 +1,1 @@
-from pywhispercpp.model import Model
-
-m = Model("assets/models/ggml-large-v2.bin", gpu=True)
-
-target_file = r"C:\Users\peter\Videos\audio-recording-1.m4a"
-transcription = m.transcribe(target_file)
-
-print(transcription.text)
+model_path = "assets/model/ggml-large-v2.bin"
