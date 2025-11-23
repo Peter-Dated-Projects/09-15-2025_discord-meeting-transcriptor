69d3f0933914ac76f1a324e6c5e9b4ef0f4dc5eb
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2025-11-23 12:39:12 -0500

    ft. create separate manager for vector reranker

--- source/services/gpu_resource_manager/vector_reranker/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Vector Reranker initialization."""
-
-from source.services.gpu_resource_manager.vector_reranker.manager import (
-    VectorRerankerManager,
-)
-
-__all__ = [
-    "VectorRerankerManager",
-]
---
+++ source/services/vector_reranker_manager/__init__.py
@@ -0,0 +1,7 @@
+"""Vector Reranker Manager Service."""
+
+from source.services.vector_reranker_manager.manager import VectorRerankerManager
+
+__all__ = [
+    "VectorRerankerManager",
+]
