a6dbc14721f5178ea30237dd95fe251cb38238c1
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2025-11-23 12:37:34 -0500

    ft. create reranker job

---
+++ source/services/gpu_resource_manager/vector_reranker/__init__.py
@@ -0,0 +1,9 @@
+"""Vector Reranker initialization."""
+
+from source.services.gpu_resource_manager.vector_reranker.manager import (
+    VectorRerankerManager,
+)
+
+__all__ = [
+    "VectorRerankerManager",
+]
